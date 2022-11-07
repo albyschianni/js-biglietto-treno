f58677c5dd16dc6ffcf24b91d19dd49500f54f1e
albyschianni <alberto.schianni@gmail.com>
2022-11-07 17:03:45 +0100

    update

--- js/text.js
+++ js/text.js
@@ -15,16 +15,24 @@
 
 // calcolo prezzo con variabili (se under 18 anni -20%, se over 65 -40% , altrimenti prezzo base 0,21 centesimi al km)
 
-    let prezzo = chilometri * 0.21
+    let prezzoNormale = chilometri * 0.21
 
-    console.log(prezzo);
+    console.log(prezzoNormale);
+
+    var prezzoFinale
 
     if (eta < 18 ){
+        prezzoFinale = prezzoNormale - (prezzoNormale * .20);
 
     } else if (eta > 65) {
+        prezzoFinale = prezzoNormale - (prezzoNormale * .40);
 
     } else {
-
+        prezzoFinale = prezzoNormale
     }
+
+    console.log(prezzoFinale)
+
+    document.getElementById("text").innerHTML = "Il prezzo del tuo biglietto è di prezzoFinale €"
 
 // output on screen del prezzo del biglietto (da sistemare con solo 2 decimalis)
